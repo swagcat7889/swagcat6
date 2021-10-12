c2576d51ed5508f2c2c0c934262c50e52cac3bd5
cola cat <86305856+swagcat7889@users.noreply.github.com>
2021-10-12 18:12:01 +0200

    Delete swagtest.js

--- swagtest.js
+++
@@ -1,6 +0,0 @@
-var fetchUrl = require("fetch").fetchUrl;
-  fetchUrl(`https://evergene.io/api/animemes`, function(error, meta, body){
-    const JSONa = body
-    const ParsedJSON = JSON.parse(JSONa);
-    console.log(ParsedJSON,0,data.children,0,data.glidings.url_overridden_by_dest);
-  });
