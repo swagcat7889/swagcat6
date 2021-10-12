f702b1b3ef75aee449016191f1f080223d690b34
cola cat <86305856+swagcat7889@users.noreply.github.com>
2021-10-12 22:12:43 +0200

    Delete sharding

--- bot.js
+++
@@ -1,7 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const { token } = require(`./config.json`)
-const manager = new ShardingManager('./non-shard.js', { token: `${token}`, shardCount: 25 });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-
-manager.spawn(3);
