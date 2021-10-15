15914ae1794aa444d7329aa91cbd7b79af64ce19
cola cat <86305856+swagcat7889@users.noreply.github.com>
2021-10-15 13:06:14 +0200

    Update help.js

--- komendy/help.js
+++ komendy/help.js
@@ -12,14 +12,48 @@ module.exports = {
     const value = interaction.options.getString('input');
     switch (value) {
         case `help`:
-      interaction.reply('help command\nusage: /help <command>\nex. use: /help help (you are seeing this now)\nrequired permissions: `-`\nDescription:');
+      interaction.reply('help command\nusage: /help <command>\nex. use: /help help (you are seeing this now)\nrequired permissions: `-`\nDescription: helps you™️');
       break;
         case `eval`:
       interaction.reply('eval command\nusage: /eval <javascript_expression>\nex. use: /eval interaction.client.ws.ping (returns bot ping)\nrequired permissions: `Developer`\nDescription: Evaluates javascript expression');
       break;
-      default:
-          interaction.reply(`Command not documented, you might be lost...`);
+        case undefined:
+      interaction.reply('An error occurred while searching.');
+        case `httpcat`:
+          interaction.reply(`\`httpcat\` command\nusage: /httpcat <http_status_code>\ndescription: httpcat error\nex. use: /httpcat number:404\nrequired permissions: \`-\``);
           break;
+        case `dog`:
+          interaction.reply(`\`dog\` command\n usage: /dog\nex. use: /dog\ndescription: random dog\n required permissions: \`-\``);
+          break;
+        case `cat`:
+          interaction.reply(`\`cat\` command\nusage: /cat\ndescription: Random cat\nex. use: /cat\nrequired permissions: \`-\``);
+          break;
+        case `coupon`:
+          interaction.reply(`\`coupon\` command\nusage: /coupon\ndescription: it doesn't do anything, just for the fun\nrequired permissions: \`-\``);
+          break;
+        case `drake`:
+          interaction.reply(`\`drake\` command\nusage: /drake input1:test1 input2:test2\ndescription: generates an random drake meme\nrequired permissions: \`-\``);
+          break;
+        case `fox`:
+          interaction.reply(`\`fox\` command\nusage: /fox\ndescription: random fox\n usage: /fox\nrequired permissions:\`-\``);
+          break;
+        case `ping`:
+          interaction.reply(`\`ping\` command\nusage: /ping\ndescription: returns gateway latency\nusage: /ping\nrequired permissions:\`-\``);
+          break;
+        case `rickroll`:
+          interaction.reply(`\`rickroll\`\ndescription: gets you trolled\nusage: /rickroll\nrequired permissions:\`-\``);
+          break;
+        case `say`:
+          interaction.reply(`\`say\` command\ndescription: says something as you\nusage: /say\nex. use: /say input: test\nrequired permissions:\`-\``);
+          break;
+        case `triggered`:
+          interaction.reply(`\`triggered\` command\ndescription: makes you triggered, or someone else you selected...\nusage: /triggered\nex. use: /triggered target:@pass the burrito down#4261\nrequired permissions:\`-\``);
+          break;
+        case null:
+      interaction.reply('Come on man, give me the command name okay?');
+        default:
+      interaction.reply(`Command not documented, you might be lost...`);
+      break;
     }
   },
 };
